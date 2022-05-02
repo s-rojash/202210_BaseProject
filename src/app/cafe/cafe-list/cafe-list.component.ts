a5b3810a55e166fa100243cf554022f0e8db08a9
Steve Andersson Rojas Hernandez <sarojash@unandes.edu.co>
2022-05-01 19:04:53 -0500

    Se agrega funcionalidad No 3

--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -10,12 +10,14 @@ import { Component, OnInit } from '@angular/core';
 export class CafeListComponent implements OnInit {
 
   cafes: Array<Cafe> = [];
+  contador: Map<string, number> = new Map<string, number>();
 
   constructor(private cafeService: CafeService) { }
 
   getCafes(): void {
       this.cafeService.getCafes().subscribe((cafes) => {
       this.cafes = cafes;
+      this.crearteContador();
     });
   }
 
@@ -23,5 +25,17 @@ export class CafeListComponent implements OnInit {
     this.getCafes();
   }
 
+  crearteContador() {
+    this.cafes.forEach(cafe => {
+      if(this.contador.get(cafe.tipo) != null){
+        let cantidad1 = this.contador.get(cafe.tipo) as number;
+        cantidad1++;
+        this.contador.set(cafe.tipo, cantidad1)
+      }
+      else {
+        this.contador.set(cafe.tipo, 1)
+      }
+    });
+  }
 
 }
